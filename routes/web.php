a58dc84b0c86dbbb41bd22a7d3ff68bfb5c6a32e
eomars <pakatsy@ciencias.unam.mx>
2016-11-02 04:14:42 -0600

    views

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+  return view('home');
+});
+
+Route::get('auth/login', function() {
+  return view('auth.login');
+});
+
+Route::get('catalog', function() {
+  return view('catalog.index');
+});
+
+Route::get('catalog/show/{id}', function() {
+  return view('catalog.show',['id' =>$id]);
+});
+
+Route::get('catalog/create', function() {
+  return view('catalog.create');
+});
+
+Route::get('catalog/edit/{id}', function() {
+  return view('catalog.edit', ['id' => $id]);
 });
