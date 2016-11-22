784dc53c3fdc9d189b8f83b70f4c00a613718fc3
eomars <pakatsy@ciencias.unam.mx>
2016-11-22 03:34:06 -0600

    auth staging

--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,6 @@ Route::get('catalog/create', 'CatalogController@getCreate');
 
 Route::get('catalog/edit/{id}', 'CatalogController@getEdit');
 
-//Rutas para autenticacion
-Route::get('auth/login','Auth\AuthController@getLogin');
-Route::post('auth/login','Auth\AuthController@postLogin');
-Route::get('auth/logout','Auth\AuthController@getLogout');
-
-//Rutas para registro
-Route::get('auth/register','Auth\AuthController@getRegister');
-Route::post('auth/register','Auth\AuthController@postRegister');
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
